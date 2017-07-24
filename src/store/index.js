0182045ec882b9dbcfcfd575412bc92bbc42714e
Jack.Zhong <Jack.Zhong@MotionGlobal.com>
2017-07-24 18:25:30 +0800

    add Vuex

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+let store = new Vuex.Store({
+	state:{
+		
+	},
+	mutations:{
+		
+	},
+	actions:{
+		
+	}
+})
+
+export default store
