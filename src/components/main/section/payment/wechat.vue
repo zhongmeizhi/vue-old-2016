8e4b9075d43d9cf4fe54f3fde58aed1290d67439
zhongmeizhi <330447171@qq.com>
2017-07-29 10:55:26 +0800

    zip picture

---
+++ src/components/main/section/payment/wechat.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<img src="/static/books/w.jpg"/>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+	div{
+		margin-top: 3.3rem;
+	}
+	img{
+		width: 100%;
+	}
+</style>
