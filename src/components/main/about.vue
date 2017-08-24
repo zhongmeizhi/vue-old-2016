c39807a64f49c1c55d33e6679ac049bab0b75372
Jack.Zhong <Jack.Zhong@MotionGlobal.com>
2017-08-24 11:41:13 +0800

    Revert "change baidu map. Make it became a slot"
    This reverts commit e72e57ce6332b839a80096555a6d351aafbcfb92.

--- src/components/main/about.vue
+++ src/components/main/about.vue
@@ -1,11 +1,6 @@
 <template>
 	<section class="about"	>
-		<h1>
-			Hi. My name is Jack. <br />
-			<a href="https://github.com/zhongmeizhi" class="gitHub">
-				Into My GitHub
-			</a>
-		</h1>
+		<h1>Hi. My name is Jack.</h1>
 		<h2>{{msg}}</h2>
 	</section>
 </template>
@@ -49,8 +44,5 @@ export default{
 		min-height: 6rem;
 		text-indent: 4rem;
 		color: goldenrod;
-	}
-	.gitHub{
-		color: #7ec801;
 	}
 </style>
--- src/components/main/eatery.vue
+++ src/components/main/eatery.vue
@@ -1,19 +1,6 @@
 <template>
 	<section class="about"	>
-		<baidu-map>
-			<div id="driving_way" slot="traffic">
-				<input type="start" name="start" id="start" v-model="start"/>
-				<input type="end" name="end" id="end" v-model="end"/>
-				<select>
-					<option value="0">最少时间</option>
-					<option value="1">最少换乘</option>
-					<option value="2">最少步行</option>
-					<option value="3">不乘地铁</option>
-				</select>
-				<input type="button" id="result" value="查询"/>
-			</div>
-			<input slot="mapSearch" type="text" id="mapSearch" name="mapSearch" placeholder="Where do you want to go..." v-model="mapInput" class="mapSearch" mapInput="mapInput">
-		</baidu-map>
+		<baidu-map></baidu-map>
 	</section>
 </template>
 
@@ -23,9 +10,6 @@ import baiduMap from '@/components/common/map'
 export default{
 	data(){
 		return {
-			mapInput: "",
-			start: "",
-			end: ""
 		}
 	},
 	components:{
