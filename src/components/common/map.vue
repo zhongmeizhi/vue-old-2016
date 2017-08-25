8655445e61d2b7c489bbaf7694f5931cb62f37e4
Jack.Zhong <Jack.Zhong@MotionGlobal.com>
2017-08-25 15:54:07 +0800

    location cur position

--- src/components/common/map.vue
+++ src/components/common/map.vue
@@ -2,9 +2,8 @@
 	<div>
 		<!-- traffic  -->
 		<slot></slot>
-		<input type="button" id="result" value="查询" @click="traffic"/>
+		<input type="button" id="result" value="查询" @click="traffic" />
 		<!-- Search -->
-		<input type="text" id="mapSearch" name="mapSearch" placeholder="address" v-model="mapInput" class="mapSearch">
 		<div id="baidu-map" class="baidu-map"></div>
 	</div>
 </template>
@@ -13,13 +12,13 @@
 		data() {
 			return {
 				map: "",
-				mapInput: "", //详细地址
 				local: "",
 				transit: "",
-				userlocation: {
-					lng: "",
-					lat: ""
+				curPosition: {
+					lg: "",
+					la: ""
 				},
+				ico: ""
 			}
 		},
 		props: ["start", "end", "customers"],
@@ -31,7 +30,7 @@
 			})
 		},
 		methods: {
-			setMap(){
+			setMap() {
 				const key = "DD279b2a90afdf0ae7a3796787a0742e";
 				return new Promise((resolve, reject) => {
 					window.init = () => {
@@ -44,72 +43,108 @@
 					document.head.appendChild(script);
 				})
 			},
-			initMap(){
+			initMap() {
 				this.map = new BMap.Map("baidu-map"); // 创建Map实例
 				var point = new BMap.Point(121.48, 31.24); // 创建点坐标。地点上海
 				var top_right_navigation = new BMap.NavigationControl({
 					anchor: BMAP_ANCHOR_TOP_RIGHT,
 					type: BMAP_NAVIGATION_CONTROL_SMALL
 				}); //右上角
-				this.map.centerAndZoom(point, 12);
+				this.map.centerAndZoom(point, 14);
 				this.map.enableScrollWheelZoom(true);
 				this.map.addControl(top_right_navigation);
-
-//	建立一个自动完成的对象				
-				let ac = new BMap.Autocomplete({
-						"input": "mapSearch",
-						"location": this.map
-					})
-
-				ac.addEventListener("onconfirm", (e)=>{ //鼠标点击下拉列表后的事件
-					let _value = e.item.value;
-					let myValue = _value.province + _value.city + _value.district + _value.street + _value.business;
-					this.mapInput = myValue;
-					this.setPlace(myValue);
+				//	ico标记
+				this.icon = new BMap.Icon('/static/tiny/man.ico', new BMap.Size(32, 32), {
+					anchor: new BMap.Size(16, 16)
 				});
 
-//	智能搜索地点
-				this.local = new BMap.LocalSearch(this.map, 
-					{onSearchComplete: this.addMark}
-				);
+				//	智能搜索地点
+				this.local = new BMap.LocalSearch(this.map, {
+					onSearchComplete: this.addMark
+				});
 
-//	导航方式：驾车出行
-				this.transit = new BMap.DrivingRoute(this.map, 
-					{renderOptions: {map: this.map}, onSearchComplete: this.trafficComplete}
-				);
-				
+				//	导航方式：驾车出行
+				this.transit = new BMap.DrivingRoute(this.map, {
+					renderOptions: {
+						map: this.map
+					},
+					onSearchComplete: this.trafficComplete
+				});
+				//	获取当前位置
+				this.getLocation();
+				//	标记现有顾客
 				this.customersMark(this.customers);
 			},
-			addMark(){
-				this.userlocation = this.local.getResults().getPoi(0).point; //获取第一个智能搜索的结果
-				this.map.addOverlay(new BMap.Marker(this.userlocation)); //添加标注
-				if(this.mapInput){
-					//	聚焦
-					this.map.centerAndZoom(this.userlocation, 17);
+			showLocation(position) {
+				let longitude = position.coords.longitude;
+				let latitude = position.coords.latitude;
+				let gpsPoint = new BMap.Point(longitude, latitude);
+
+				let translateCallback = (data) => {
+					if(data.status === 0) {
+						let markergps = new BMap.Marker(data.points[0], {
+							icon: this.icon
+						});
+						this.map.addOverlay(markergps); //添加GPS标注  
+						this.map.setCenter(data.points[0]);
+					}
+				}
+
+				setTimeout(function() {
+					var convertor = new BMap.Convertor();
+					var pointArr = [];
+					pointArr.push(gpsPoint);
+					convertor.translate(pointArr, 1, 5, translateCallback)
+				}, 1000);
+
+			},
+			errorHandler(err) {
+				if(err.code == 1) {
+					console.error("Error: Access is denied!");
+				} else if(err.code == 2) {
+					console.error("Error: Position is unavailable!");
 				}
 			},
-			customersMark(customer){
-				if(customer.length>0){
-					customer.forEach((v, i)=>{
+			getLocation() {
+				if(navigator.geolocation) {
+					let options = {
+						timeout: 6000
+					};
+					navigator.geolocation.getCurrentPosition(this.showLocation, this.errorHandler, options);
+				} else {
+					alert("Sorry, browser does not support geolocation!");
+				}
+			},
+			addMark() {
+				let userlocation = this.local.getResults().getPoi(0).point; //获取第一个智能搜索的结果
+				let mark = new BMap.Marker(userlocation);
+				this.map.addOverlay(mark); //添加标注
+				mark.setLabel(new BMap.Label(this.local.ga.sd));
+			},
+			customersMark(customer) {
+				if(customer.length > 0) {
+					customer.forEach((v, i) => {
 						this.setPlace(v.address);
 					})
-				}else{
+				} else {
 					return;
 				}
 			},
-			setPlace(myV){
+			setPlace(myV) {
 				this.map.clearOverlays(); //清除地图上所有覆盖物
 				this.local.search(myV);
 			},
-			traffic(){
+			traffic() {
 				this.map.clearOverlays();
 				this.transit.search(this.start, this.end);
 			},
 			trafficComplete(results) {
-				if (this.transit.getStatus() != BMAP_STATUS_SUCCESS) return ;
+				if(this.transit.getStatus() != BMAP_STATUS_SUCCESS) return;
 				let plan = results.getPlan(0);
-				let output = `从${this.start}到${this.end}驾车需要${plan.getDuration(true)}${"\n"}总路程为: ${plan.getDistance(true)}`; 
-				setTimeout(function(){alert(output)},"500");
+				let output = `从${this.start}到${this.end}驾车需要${plan.getDuration(true)}${"\n"}总路程为: ${plan.getDistance(true)}`;
+				setTimeout(function() {
+					alert(output)
+				}, "500");
 			}
 		}
 	}
