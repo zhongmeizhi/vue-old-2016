f9a645c11e90f3471ba9f7c11a6c162a5b777d50
Jack.Zhong <Jack.Zhong@MotionGlobal.com>
2017-08-29 19:07:10 +0800

    翻墙不能定位到当前位置。

--- src/components/common/map.vue
+++ src/components/common/map.vue
@@ -118,12 +118,13 @@
 			errorHandler(err) {
 				if(err.code == 1) {
 					this.pop.appear = true;
-					this.pop.output = "不能定位到当前位置。";
-					alert()
+					this.pop.output = "由于你翻墙了，在并不能定位到你当前位置。</br>那么默认经纬度为121.48,31.24";
 				} else if(err.code == 2) {
 					this.pop.appear = true;
-					this.pop.output =  "当前经纬度不可用。";
+					this.pop.output =  "当前经纬度不可用。</br>那么默认经纬度为121.48,31.24";
 				}
+				this.start.lng = 121.48;
+				this.start.lat = 31.24;
 			},
 			getLocation() {
 				if(navigator.geolocation) {
